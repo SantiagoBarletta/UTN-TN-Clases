02bee9d1b3aae1c7f814e03beed152eae9f86229
SantiagoBarletta <santiagobarletta@outlook.com>
2024-05-09 02:35:23 -0300

    clase 12

---
+++ clase-12/assets/js/script.js
@@ -0,0 +1,27 @@
+// console.log('hola mundo');
+
+// console.log ( ! true);
+
+// console.log ( ! '');
+
+
+// console.log (false || 0 || 'pepa')
+
+// console.log (false && 0 && 'pepe')
+
+// console.log (4 && false || 'pepe')
+
+// console.log (8 == '8' || 1 === '1')
+
+// console.log (null === NaN || Boolean(NaN))
+
+// console.log (Number('1') === Number (1) && Number ('pepe' +1))
+
+
+var edad = 50
+
+console.log(edad)
+
+//let edad = 50
+
+
