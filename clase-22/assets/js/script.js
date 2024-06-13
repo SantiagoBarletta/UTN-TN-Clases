a132f8c11c74060a3da66fa53a1ad4e2fdb664f0
Santiago Barletta <112686341+SantiagoBarletta@users.noreply.github.com>
2024-06-13 00:44:39 -0300

    clase 22 actualizacion

--- clase-22/assets/js/script.js
+++ clase-22/assets/js/script.js
@@ -1,16 +1,35 @@
 const API_KEY = 'dd00aa6b89a4eaf22b5fbf601827b192';
 const URL_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=27&language=es-ES`;
+const URL_IMAGEN = 'https://image.tmdb.org/t/p/w500';
+const containerPeliculas = document.getElementById('container-peliculas');
 
-const respuesta = fetch(URL_API + '&page=1', {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json'
+///////////////////// PRACTICA ////////////////////////
+let peliculas = [];
+
+const obtenerPeliculas = async () => {
+        const respuesta = await fetch(URL_API + '&page=1', {
+            method: 'GET',
+        });
+        const data = await respuesta.json();
+        peliculas = data.results;
+
+        mostrarPeliculas();
+}
+
+const plantillaPelicula = (peli) => `
+    <div class="pelicula">
+        <h1>${peli.title}</h1>
+        <img src="${URL_IMAGEN + peli.poster_path}" alt="${peli.title}">
+        <p>${peli.overview}</p>
+    </div>
+`;
+
+const mostrarPeliculas = () => {
+    for (const peli of peliculas) {
+        containerPeliculas.innerHTML += plantillaPelicula(peli);
     }
-})
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
-        console.log(error);
-    });
+}
+
+obtenerPeliculas();
+
+    
