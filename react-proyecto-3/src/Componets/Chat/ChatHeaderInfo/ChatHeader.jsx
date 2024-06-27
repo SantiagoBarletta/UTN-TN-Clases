ba670694ad12928447e3bbecdf13fcf56bcfff6b
Santiago Barletta <112686341+SantiagoBarletta@users.noreply.github.com>
2024-06-27 02:44:29 -0300

    mensajeria

--- react-proyecto-3/src/Componets/Chat/ChatHeaderInfo/ChatHeader.jsx
+++ react-proyecto-3/src/Componets/Chat/ChatHeaderInfo/ChatHeader.jsx
@@ -1,10 +1,17 @@
 import React from "react"
+import './ChatHeader.css'
+import pepe from '../../../Componets/Contactos/Pepe_2018.jpg'
 
 function ChatHeader() {
-    return (
-      <div>
-        <h1>Dato contacto</h1>
-      </div>
-    )
-  }
-  export {ChatHeader}
+  return (
+    <div class="chat-header">
+      <img src={pepe} alt="Contact Picture"/>
+        <div class="contact-info">
+          <div class="contact-name">Pepe</div>
+          <div class="contact-status">Online</div>
+        </div>
+    </div>
+
+  )
+}
+export { ChatHeader }
--- react-proyecto-3/src/Screens/ChatScreens.jsx
+++ react-proyecto-3/src/Screens/ChatScreens.jsx
@@ -2,16 +2,17 @@ import React from "react"
 import { ChatHeader } from "../Componets/Chat/ChatHeaderInfo/ChatHeader"
 import { Chat } from "../Componets/Chat/ListaMensajes/ListaMensajes"
 import { NuevoMensajeForm } from "../Componets/Chat/MensajeForm/NuevoMensajeForm"
+import "./ChatScreens.css"
 
 
 function ChatScreens() {
     
     return (
-      <>
+      <div className="chat-screens">
         <ChatHeader />
         <Chat />
         <NuevoMensajeForm />
-      </>
+      </div>
     )
   }
   export default ChatScreens
